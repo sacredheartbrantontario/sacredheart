80f6c8f1fe02e9a021dbf35733a407d754090d2e
sacredheartbrantontario <sacredheartbrantontario@gmail.com>
2026-04-22 10:57:40 -0400

    Delete nav.js

--- nav.js
+++
@@ -1,6 +0,0 @@
-const toggle = document.getElementById("nav-toggle");
-const menu = document.getElementById("nav-menu");
-
-toggle.addEventListener("click", () => {
-  menu.style.display = menu.style.display === "flex" ? "none" : "flex";
-});
